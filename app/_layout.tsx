26703e779e027dca1b6232d46a7d77e2cee9dcc1
Arman Mamyan <armmamyan@gmail.com>
2025-07-30 00:14:17 +0400

    Add Android related custom configs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
-import "react-native-get-random-values";
-// Add global shims
 import "@account-kit/react-native";
 import 'react-native-reanimated';
+import Constants from "expo-constants";
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -10,7 +9,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
-  console.log(process.env.EXPO_PUBLIC_ALCHEMY_API_KEY)
+  console.log(Constants.expoConfig?.extra?.EXPO_PUBLIC_ALCHEMY_API_KEY)
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
